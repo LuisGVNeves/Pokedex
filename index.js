59f732842aa152f7c71b18552c7675d7c3493c81
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-05-27 12:46:01 -0300

    Feat(Import do express, adição do array pokedex)

---
+++ index.js
@@ -0,0 +1,61 @@
+// # Importar express
+const express = require('express');
+
+// # Invocar express
+const app = express();
+
+// # Biblioteca pra utilizar caminhos
+const path = require('path');
+
+// # Setar view do projeto pra EJS
+app.set("view engine", "ejs");
+
+// # Express vai utilizar arquivos estáticos da pasta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(express.urlencoded());
+
+// # Array pokedex que vai armazenar input
+const pokedex = [
+	{
+		altura: '0.7 m',
+		peso: '6.9 kg',
+		category: 'Semente',
+		name: 'Bulbasaur',
+		description:'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+		type: ['Envenenamento','Combustão'],
+		Fraqueza: ['Fogo', 'Não Voa', 'Não solta gelo'],
+		image:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+	},
+	{
+		altura: '1.1 m',
+		peso: '19.0 kg',
+		category: 'chamas',
+		name: 'Charmeleon',
+		description:'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
+		type: 'fogo',
+		Fraqueza: ['agua', 'Não Voa', 'Não solta gelo'],
+		image:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+	},
+	{
+		altura: '0.7 m',
+		peso: '7.5 kg',
+		category: 'água',
+		name: 'Wartortle',
+		description:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+		type: ['Envenenamento','Combustão'],
+		Fraqueza: ['semente', 'eletricidade'],
+		image:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+	},
+	{
+		altura: '3 m',
+		peso: '65 kg',
+		category: 'veneno',
+		name: 'Arbok',
+		description:'The frightening patterns on its belly have been studied. Six variations have been confirmed.',
+		type: ['Envenenamento','Combustão'],
+		Fraqueza: ['ground', 'Não Voa', 'Não solta gelo'],
+		image:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/024.png',
+	},
+];
